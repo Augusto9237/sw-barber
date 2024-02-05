7602781cdc25dc5728eadec679fe9f99289c8493
Augusto9237 <augusto.souza8330@gmail.com>
2024-02-04 21:43:06 -0300

    feat: add ServiceItem component

---
+++ app/barbershop/[id]/_components/barbershop-info.tsx
@@ -0,0 +1,57 @@
+'use client'
+import Image from "next/image"
+import { Button } from "@/app/_components/ui/button"
+import { ChevronLeftIcon, MenuIcon, MapPinIcon, StarIcon } from "lucide-react"
+import { Barbershop } from "@prisma/client"
+import { useRouter } from "next/navigation"
+
+interface BarbershopProps {
+    barbershop: Barbershop
+}
+
+export default function BarbershopInfo({ barbershop }: BarbershopProps) {
+    const router = useRouter()
+
+    function handleBackClick() {
+        router.back()
+    }
+    return (
+
+        <div>
+            <div className='h-[250px] w-full relative'>
+                <Button onClick={handleBackClick} size="icon" variant="outline" className='absolute z-50 top-4 left-4'>
+                    <ChevronLeftIcon />
+                </Button>
+
+                <Button size="icon" variant="outline" className='absolute z-50 top-4 right-4'>
+                    <MenuIcon />
+                </Button>
+
+                <Image
+                    src={barbershop.imageUrl}
+                    alt={barbershop.name}
+                    fill
+                    style={{
+                        objectFit: 'cover'
+                    }}
+                />
+            </div>
+
+            <div>
+                <div className='px-5 pt-3 pb-6 border-b border-solid border-secondary'>
+                    <h1 className='text-xl font-bold'> {barbershop.name}</h1>
+
+                    <div className="flex items-center gap-1 mt-2">
+                        <MapPinIcon className='text-primary' size={18} />
+                        <p className='text-sm'>{barbershop.address}</p>
+                    </div>
+
+                    <div className="flex items-center gap-1 mt-2">
+                        <StarIcon className='text-primary' size={18} />
+                        <p className='text-sm'>5,0 (800 avaliações)</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- app/barbershop/[id]/page.tsx
+++ app/barbershop/[id]/page.tsx
@@ -3,6 +3,8 @@ import { db } from '@/app/_lib/prisma';
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from 'lucide-react';
 import Image from 'next/image';
 import React from 'react'
+import BarbershopInfo from './_components/barbershop-info';
+import ServiceItem from './_components/service-item';
 
 interface BarbershopDetailsProps {
     params: {
@@ -17,6 +19,9 @@ export default async function BarbershopDetails({ params }: BarbershopDetailsPro
     const barbershop = await db.barbershop.findUnique({
         where: {
             id: params.id
+        },
+        include: {
+            services: true,
         }
     })
 
@@ -26,39 +31,12 @@ export default async function BarbershopDetails({ params }: BarbershopDetailsPro
 
     return (
         <div>
-            <div className='h-[250px] w-full relative'>
-                <Button size="icon" variant="outline" className='absolute z-50 top-4 left-4'>
-                    <ChevronLeftIcon />
-                </Button>
+            <BarbershopInfo barbershop={barbershop} />
 
-                <Button size="icon" variant="outline" className='absolute z-50 top-4 right-4'>
-                    <MenuIcon />
-                </Button>
-
-                <Image
-                    src={barbershop.imageUrl}
-                    alt={barbershop.name}
-                    fill
-                    style={{
-                        objectFit: 'cover'
-                    }}
-                />
-            </div>
-
-            <div>
-                <div className='px-5 pt-3 pb-6 border-b border-solid border-secondary'>
-                    <h1 className='text-xl font-bold'> {barbershop.name}</h1>
-
-                    <div className="flex items-center gap-1 mt-2">
-                        <MapPinIcon className='text-primary' size={18} />
-                        <p className='text-sm'>{barbershop.address}</p>
-                    </div>
-
-                    <div className="flex items-center gap-1 mt-2">
-                        <StarIcon className='text-primary' size={18} />
-                        <p className='text-sm'>5,0 (800 avaliações)</p>
-                    </div>
-                </div>
+            <div className='px-5 py-6 flex flex-col gap-3'>
+                {barbershop?.services.map((service) => (
+                    <ServiceItem key={service.id} service={service} />
+                ))}
             </div>
         </div>
     )
