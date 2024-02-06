c1beca6fcbea031b6fe1f1b05e45c5822e69d6f9
Augusto9237 <augusto.souza8330@gmail.com>
2024-02-06 01:32:56 -0300

    feat: add sheet component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -3,13 +3,22 @@
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon, MenuIcon, UserIcon } from "lucide-react";
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
-import { useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import { Avatar, AvatarImage } from "./ui/avatar";
+import Link from "next/link";
 
 export default function Header() {
-    const { data, status } = useSession()
+    const { data, status } = useSession();
+
+    function handleLoginClick() {
+        signIn('google')
+    }
+
+    function handleLogoutClick() {
+        signOut()
+    }
 
     return (
         <Card className="rounded-none">
@@ -31,12 +40,51 @@ export default function Header() {
                         </SheetHeader>
 
                         {status === "authenticated" && (
-                            <div className="flex items-center gap-3">
-                                <Avatar>
-                                    <AvatarImage src={data.user?.image ?? ''} />
-                                </Avatar>
+                            <div className="flex justify-between items-center px-5 py-6">
+                                <div className="flex items-center gap-3">
+                                    <Avatar>
+                                        <AvatarImage src={data.user?.image ?? ''} />
+                                    </Avatar>
+
+                                    <h2 className="font-bold">{data.user?.name}</h2>
+                                </div>
+                                <Button onClick={handleLogoutClick} variant="secondary" size="icon">
+                                    <LogOutIcon />
+                                </Button>
                             </div>
                         )}
+
+                        {status === "unauthenticated" && (
+                            <div className="px-5 py-6 flex flex-col gap-3">
+                                <div className="flex items-center gap-2">
+                                    <UserIcon size={32} />
+                                    <h2 className="font-bold">Olá, faça seu login!</h2>
+                                </div>
+                                <Button onClick={handleLoginClick} variant="secondary" className="w-full flex gap-2 justify-start">
+                                    <LogInIcon size={18} />
+                                    Fazer Login
+                                </Button>
+                            </div>
+                        )}
+
+                        <div className="flex flex-col gap-3 px-5">
+
+                            <Button variant="outline" className="justify-start gap-2" asChild>
+                                <Link href="/">
+                                    <HomeIcon size={18} />
+                                    Inicio
+                                </Link>
+                            </Button>
+
+                            {data?.user && (
+                                <Button variant="outline" className="justify-start gap-2" asChild>
+                                    <Link href="/bookings">
+                                        <CalendarIcon size={18} />
+                                        Agendamentos
+                                    </Link>
+                                </Button>
+                            )}
+                        </div>
                     </SheetContent>
                 </Sheet>
             </CardContent>
