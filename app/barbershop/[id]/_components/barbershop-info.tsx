f8078d59adb4118a6dbb50568375370e778334f5
Augusto9237 <augusto.souza8330@gmail.com>
2024-03-06 15:08:17 -0300

    feat: ensure unauthenticated user is redirected to login on booking click

--- app/barbershop/[id]/_components/barbershop-info.tsx
+++ app/barbershop/[id]/_components/barbershop-info.tsx
@@ -8,26 +8,17 @@ import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/ap
 import { signIn, signOut, useSession } from "next-auth/react"
 import { Avatar, AvatarImage } from "@/app/_components/ui/avatar"
 import Link from "next/link"
+import SideMenu from "@/app/_components/side-menu"
 
 interface BarbershopProps {
     barbershop: Barbershop
 }
 
 export default function BarbershopInfo({ barbershop }: BarbershopProps) {
-    const { data, status } = useSession();
-
-    function handleLoginClick() {
-        signIn('google')
-    }
-
-    function handleLogoutClick() {
-        signOut()
-    }
-    
     const router = useRouter()
 
     function handleBackClick() {
-        router.back()
+        router.replace("/")
     }
     return (
 
@@ -37,8 +28,6 @@ export default function BarbershopInfo({ barbershop }: BarbershopProps) {
                     <ChevronLeftIcon />
                 </Button>
 
-                
-
                 <Sheet>
                     <SheetTrigger>
                         <Button size="icon" variant="outline" className='absolute z-50 top-4 right-4'>
@@ -46,60 +35,7 @@ export default function BarbershopInfo({ barbershop }: BarbershopProps) {
                         </Button>
                     </SheetTrigger>
                     
-                    <SheetContent className="p-0">
-                        <SheetHeader className="text-left border-b border-solid border-secondary p-5">
-                            <SheetTitle>
-                                Menu
-                            </SheetTitle>
-                        </SheetHeader>
-
-                        {data?.user && status === "authenticated" && (
-                            <div className="flex justify-between items-center px-5 py-6">
-                                <div className="flex items-center gap-3">
-                                    <Avatar>
-                                        <AvatarImage src={data.user?.image ?? ''} />
-                                    </Avatar>
-
-                                    <h2 className="font-bold">{data.user?.name}</h2>
-                                </div>
-                                <Button onClick={handleLogoutClick} variant="secondary" size="icon">
-                                    <LogOutIcon />
-                                </Button>
-                            </div>
-                        )}
-
-                        {status === "unauthenticated" && (
-                            <div className="px-5 py-6 flex flex-col gap-3">
-                                <div className="flex items-center gap-2">
-                                    <UserIcon size={32} />
-                                    <h2 className="font-bold">Olá, faça seu login!</h2>
-                                </div>
-                                <Button onClick={handleLoginClick} variant="secondary" className="w-full flex gap-2 justify-start">
-                                    <LogInIcon size={18} />
-                                    Fazer Login
-                                </Button>
-                            </div>
-                        )}
-
-                        <div className="flex flex-col gap-3 px-5">
-
-                            <Button variant="outline" className="justify-start gap-2" asChild>
-                                <Link href="/">
-                                    <HomeIcon size={18} />
-                                    Inicio
-                                </Link>
-                            </Button>
-
-                            {data?.user && (
-                                <Button variant="outline" className="justify-start gap-2" asChild>
-                                    <Link href="/bookings">
-                                        <CalendarIcon size={18} />
-                                        Agendamentos
-                                    </Link>
-                                </Button>
-                            )}
-                        </div>
-                    </SheetContent>
+                    <SideMenu/>
                 </Sheet>
 
                 <Image
